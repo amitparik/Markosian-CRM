e07b15eebf53c9a6e2e711650d4773e463b8a928
Ananad Murti <anandmurti.mre@gmail.com>
2022-07-06 18:47:35 +0530

    login page setup

--- src/routes/MainRoutes.js
+++ src/routes/MainRoutes.js
@@ -1,3 +1,22 @@
-/**
- * Created by Win10 on 05-07-2022.
- */
+import React,{lazy}  from 'react'
+import {
+    BrowserRouter,
+    Routes,
+    Route,
+  } from "react-router-dom";
+//import Login from '../containers/Login/Login';
+
+const Login = lazy(() => import('../containers/Login/Login'));
+// const Registration = lazy(() => import('../Container/LoginRegistration/Registration/Registration'))
+
+export default function MainRoutes() {
+  return (
+    <>  
+        <BrowserRouter>
+            <Routes>
+                <Route exact path="/"  element={<Login />} />
+            </Routes>
+        </BrowserRouter>
+    </>
+  )
+}
